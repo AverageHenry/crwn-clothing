e7f630629bb5bd3b43aca8e772e6dff1484c7a96
Henry Adugba <hkadugba.dev@gmail.com>
2020-07-12 17:11:41 -0400

    Created project with create-react-app and added files for homepage.

---
+++ src/directory-menu.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { MenuItem } from './menu-item.component';
+
+export const DirectoryMenu = () => (
+    <div className="directory-menu">
+        <MenuItem title="HATS" subtitle="SHOP NOW" />
+        <MenuItem title="JACKETS" subtitle="SHOP NOW" />
+        <MenuItem title="SNEAKERS" subtitle="SHOP NOW" />
+        <MenuItem title="WOMENS" subtitle="SHOP NOW" />
+        <MenuItem title="MENS" subtitle="SHOP NOW" />
+    </div>
+);
---
+++ src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { DirectoryMenu } from './directory-menu.component';
+
+import './homepage.styles.scss';
+
+export const Homepage = () => (
+    <div className="homepage">
+        <DirectoryMenu />
+    </div>
+);
