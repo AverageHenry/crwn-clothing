e10077ac960bea95f00e2c32447d47175078fbd5
Henry Adugba <hkadugba.dev@gmail.com>
2020-07-13 18:18:16 -0400

    Added shop page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-export const MenuItem = ({ title, subtitle, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
             <div
                 className="background-image"
                 style={{ backgroundImage: `url(${imageUrl})` }}
             ></div>
             <div className="content">
                 <h1 className="title">{title}</h1>
-                <span className="subtitle">{subtitle}</span>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
     );
 };
+
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,8 +4,10 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
-export const Homepage = () => (
+const Homepage = () => (
     <div className="homepage">
         <Directory />
     </div>
 );
+
+export default Homepage;
